e4009d99e285a1166c7d42ceaed5a6ab62f3eda6
Viethoang1109 <vhoang1928@gmail.com>
2025-01-09 23:51:29 +0700

    Update api volunteer

--- feature/volunteer/dto/volunteer_dto.go
+++ feature/volunteer/dto/volunteer_dto.go
@@ -1,19 +1,37 @@
 package dto
 
 type VolunteerCreateDTO struct {
-	UserID       int `json:"user_id" binding:"required"`
-	DepartmentID int `json:"department_id" binding:"required"`
-	Status       int `json:"status" binding:"required"`
+	UserID             int    `json:"user_id" binding:"required"`
+	DepartmentID       int    `json:"department_id" binding:"required"`
+	Status             int    `json:"status" binding:"required"`
+	Dob                string `json:"dob"`
+	Mobile             string `json:"mobile"`
+	CountryID          int    `json:"country_id"`
+	ResidentCountryID  int    `json:"resident_country_id"`
+	Avatar             string `json:"avatar"`
+	VerificationStatus int    `json:"verification_status"`
 }
 
 type VolunteerUpdateDTO struct {
-	DepartmentID int `json:"department_id"`
-	Status       int `json:"status"`
+	DepartmentID       int    `json:"department_id"`
+	Dob                string `json:"dob"`
+	Mobile             string `json:"mobile"`
+	CountryID          int    `json:"country_id"`
+	ResidentCountryID  int    `json:"resident_country_id"`
+	Avatar             string `json:"avatar"`
+	VerificationStatus int    `json:"verification_status"`
+	Status             int    `json:"status"`
 }
 
 type VolunteerResponseDTO struct {
-	ID           int `json:"id"`
-	UserID       int `json:"user_id"`
-	DepartmentID int `json:"department_id"`
-	Status       int `json:"status"`
+	ID                 int    `json:"id"`
+	UserID             int    `json:"user_id"`
+	DepartmentID       int    `json:"department_id"`
+	Dob                string `json:"dob"`
+	Mobile             string `json:"mobile"`
+	CountryID          int    `json:"country_id"`
+	ResidentCountryID  int    `json:"resident_country_id"`
+	Avatar             string `json:"avatar"`
+	VerificationStatus int    `json:"verification_status"`
+	Status             int    `json:"status"`
 }
