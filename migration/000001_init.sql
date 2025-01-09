9ba53cedfc8612bb44bff60044ef9704f93a0972
Viethoang1109 <vhoang1928@gmail.com>
2025-01-09 23:46:43 +0700

    Update postgres database

--- migration/000001_init.sql
+++ migration/000001_init.sql
@@ -4,7 +4,6 @@
 CREATE TABLE IF NOT EXISTS roles (
     id SERIAL PRIMARY KEY,
     name VARCHAR(45) NOT NULL,
-    status VARCHAR(30) CHECK (status IN ('pending', 'approved', 'rejected')) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
@@ -29,35 +28,32 @@ CREATE TABLE IF NOT EXISTS countries (
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     role_id INT NOT NULL,
-    department_id INT,
     email VARCHAR(45) NOT NULL,
     password TEXT NOT NULL,
     name VARCHAR(45) NOT NULL,
     surname VARCHAR(45) NOT NULL,
     gender VARCHAR(20) NOT NULL,
-    dob DATE NOT NULL,
-    mobile VARCHAR(15) NOT NULL,
-    country_id INT NOT NULL,
-    resident_country_id INT NOT NULL,
-    avatar VARCHAR(100),
-    verification_status SMALLINT DEFAULT 0, -- 0: unverified, 1: verified
-    status SMALLINT NOT NULL, -- 0: inactive, 1: active
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT fk_users_roles FOREIGN KEY (role_id) REFERENCES roles (id),
-    CONSTRAINT fk_users_countries FOREIGN KEY (country_id) REFERENCES countries (id),
-    CONSTRAINT fk_users_resident_countries FOREIGN KEY (resident_country_id) REFERENCES countries (id),
-    CONSTRAINT fk_users_departments FOREIGN KEY (department_id) REFERENCES departments (id)
+    CONSTRAINT fk_users_roles FOREIGN KEY (role_id) REFERENCES roles (id)
 );
 
 CREATE TABLE IF NOT EXISTS volunteer_details (
     id SERIAL PRIMARY KEY,
     user_id INT NOT NULL,
     department_id INT NOT NULL,
+    dob DATE NOT NULL,
+    mobile VARCHAR(15) NOT NULL,
+    country_id INT NOT NULL,
+    resident_country_id INT NOT NULL,
+    avatar VARCHAR(100),
+    verification_status SMALLINT DEFAULT 0, -- 0: unverified, 1: verified
     status SMALLINT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT fk_volunteer_details_users FOREIGN KEY (user_id) REFERENCES users (id),
+    CONSTRAINT fk_volunteer_details_countries FOREIGN KEY (country_id) REFERENCES countries (id),
+    CONSTRAINT fk_volunteer_details_resident_countries FOREIGN KEY (resident_country_id) REFERENCES countries (id),
     CONSTRAINT fk_volunteer_details_departments FOREIGN KEY (department_id) REFERENCES departments (id)
 );
 
