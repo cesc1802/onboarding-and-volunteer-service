e2fa9072d5171b3b36497d4a580903277eac6c44
Viethoang1109 <vhoang1928@gmail.com>
2025-01-09 23:49:48 +0700

    Update api user authentication

--- feature/authentication/storage/user_repository.go
+++ feature/authentication/storage/user_repository.go
@@ -1,9 +1,7 @@
 package storage
 
 import (
-	"errors"
 	"log"
-	"time"
 
 	"github.com/cesc1802/onboarding-and-volunteer-service/feature/authentication/domain"
 	"github.com/cesc1802/onboarding-and-volunteer-service/feature/authentication/dto"
@@ -33,9 +31,9 @@ func (r *AuthenticationRepository) GetUserByEmail(email string, password string)
 		}
 		return nil, err.Error()
 	}
-	if user.Status == 0 {
-		return nil, "User is inactive"
-	}
+	// if user.Status == 0 {
+	// 	return nil, "User is inactive"
+	// }
 	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)); err != nil {
 		return nil, "Password is incorrect"
 	}
@@ -55,27 +53,28 @@ func (r *AuthenticationRepository) RegisterUser(request *dto.RegisterUserRequest
 		return nil, err
 	}
 
-	dob, err := time.Parse("2006-01-02", request.Dob) // Expected format: YYYY-MM-DD
-	if err != nil {
-		log.Println("Invalid Dob format:", request.Dob)
-		return nil, errors.New("invalid date of birth format, expected YYYY-MM-DD")
-	}
-
+	// dob, err := time.Parse("2006-01-02", request.Dob) // Expected format: YYYY-MM-DD
+	// if err != nil {
+	// 	log.Println("Invalid Dob format:", request.Dob)
+	// 	return nil, errors.New("invalid date of birth format, expected YYYY-MM-DD")
+	// }
+	roleID := 2
 	user := domain.User{
-		Email:              request.Email,
-		Name:               request.Name,
-		Surname:            request.Surname,
-		Password:           string(hashedPassword),
-		Gender:             request.Gender,
-		Dob:                dob,
-		Mobile:             request.Mobile,
-		CountryID:          &request.CountryID,
-		ResidentCountryID:  &request.ResidentCountryID,
-		Avatar:             request.Avatar,
-		VerificationStatus: request.VerificationStatus,
-		Status:             request.Status,
-		RoleID:             &request.RoleID,
-		DepartmentID:       &request.DepartmentID,
+		Email:    request.Email,
+		Name:     request.Name,
+		Surname:  request.Surname,
+		Password: string(hashedPassword),
+		Gender:   request.Gender,
+		RoleID:   &roleID,
+		// Dob:                dob,
+		// Mobile:             request.Mobile,
+		// CountryID:          &request.CountryID,
+		// ResidentCountryID:  &request.ResidentCountryID,
+		// Avatar:             request.Avatar,
+		// VerificationStatus: request.VerificationStatus,
+		// Status:             request.Status,
+		// RoleID:             &request.RoleID,
+		// DepartmentID:       &request.DepartmentID,
 	}
 
 	if err := r.db.Create(&user).Error; err != nil {
@@ -87,21 +86,21 @@ func (r *AuthenticationRepository) RegisterUser(request *dto.RegisterUserRequest
 	response := &dto.RegisterUserResponse{
 		Message: "User registered successfully",
 		User: &dto.RegisterUserRequest{
-			ID:                 user.ID,
-			Email:              user.Email,
-			Name:               user.Name,
-			Password:           user.Password,
-			Surname:            user.Surname,
-			Gender:             user.Gender,
-			Dob:                user.Dob.Format("11/01/2000"),
-			Mobile:             user.Mobile,
-			Avatar:             user.Avatar,
-			CountryID:          *user.CountryID,
-			ResidentCountryID:  *user.ResidentCountryID,
-			VerificationStatus: user.VerificationStatus,
-			Status:             user.Status,
-			RoleID:             *user.RoleID,
-			DepartmentID:       *user.DepartmentID,
+			ID:       user.ID,
+			Email:    user.Email,
+			Name:     user.Name,
+			Password: user.Password,
+			Surname:  user.Surname,
+			Gender:   user.Gender,
+			// Dob:                user.Dob.Format("11/01/2000"),
+			// Mobile:             user.Mobile,
+			// Avatar:             user.Avatar,
+			// CountryID:          *user.CountryID,
+			// ResidentCountryID:  *user.ResidentCountryID,
+			// VerificationStatus: user.VerificationStatus,
+			// Status:             user.Status,
+			// RoleID:             *user.RoleID,
+			// DepartmentID:       *user.DepartmentID,
 		},
 	}
 
